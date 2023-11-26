64b83235bfeb32d98271103badf6c4f9bf818707
Wesley Pereira <jimmmisss@gmail.com>
2023-11-26 10:21:05 -0300

    Full Cycle 3 - Application layer tests use case delete category

--- application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryUseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryUseCase.java
@@ -1,7 +1,6 @@
 package com.fullcycle.admin.catalogo.application.category.delete;
 
 import com.fullcycle.admin.catalogo.application.UnitUseCase;
-import com.fullcycle.admin.catalogo.application.UseCase;
 
 public abstract class DeleteCategoryUseCase extends UnitUseCase<String> {
 }
