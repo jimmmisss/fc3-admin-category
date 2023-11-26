1fd688780f9369de622191bb606c41a013d6e10e
Wesley Pereira <jimmmisss@gmail.com>
2023-11-26 12:49:53 -0300

    Full Cycle 3 - Application layer tests use case list categories

---
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,22 @@
+package com.fullcycle.admin.catalogo.application.category.retrieve.list;
+
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.Objects;
+
+public class DefaultListCategoriesUseCase extends ListCategoriesUseCase {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultListCategoriesUseCase(final CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public Pagination<CategoryListOutput> execute(final SearchQuery anIn) {
+        return this.categoryGateway.findAll(anIn)
+                .map(CategoryListOutput::from);
+    }
+}
