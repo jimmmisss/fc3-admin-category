2626fac7a7105bd5ab949d372876fc280f7a8851
Wesley Pereira <jimmmisss@gmail.com>
2023-11-29 11:12:11 -0300

    Full Cycle 3 - Infrastructure layer database

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -9,7 +9,7 @@ import org.springframework.core.env.AbstractEnvironment;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "production");
         SpringApplication.run(WebServer.class, args);
     }
 }
