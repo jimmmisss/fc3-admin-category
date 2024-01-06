9c5957823c247e55e1458f9a5382b43f3116150c
Wesley Pereira <jimmmisss@gmail.com>
2024-01-06 16:55:25 -0300

    Full Cycle 3 - API layer category - test and resource create category

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -0,0 +1,43 @@
+package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
+
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryCommand;
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryOutput;
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
+import com.fullcycle.admin.catalogo.infrastructure.category.models.CreateCategoryRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.function.Function;
+
+@RestController
+public class CategoryController implements CategoryAPI {
+
+    @Autowired
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = createCategoryUseCase;
+    }
+
+    @Override
+    public ResponseEntity<?> createCategory(CreateCategoryRequest input) {
+        final var aCommand = CreateCategoryCommand.with(
+                input.name(),
+                input.description(),
+                input.active() != null ? input.active() : true
+        );
+
+        final Function<Notification, ResponseEntity<?>> onError = notification ->
+                ResponseEntity.unprocessableEntity().body(notification);
+
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess = output ->
+                ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
+
+        return this.createCategoryUseCase.execute(aCommand)
+                .fold(onError, onSuccess);
+
+    }
+}
--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/ControllerTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/ControllerTest.java
@@ -3,6 +3,7 @@ package com.fullcycle.admin.catalogo;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.ObjectMapperConfig;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
@@ -14,4 +15,7 @@ import java.lang.annotation.*;
 @WebMvcTest
 @Import(ObjectMapperConfig.class)
 public @interface ControllerTest {
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "controllers")
+    Class<?>[] controllers() default {};
 }
