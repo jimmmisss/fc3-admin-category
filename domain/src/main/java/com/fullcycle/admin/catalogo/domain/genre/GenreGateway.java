05db8739ec7264c7a1d4bd6386d5405ad23b37ef
Wesley Pereira <jimmmisss@gmail.com>
2024-01-11 16:13:09 -0300

    Full Cycle 3 - Domain Genre - Gateway

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/genre/GenreGateway.java
@@ -0,0 +1,20 @@
+package com.fullcycle.admin.catalogo.domain.genre;
+
+import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
+
+import java.util.Optional;
+
+public interface GenreGateway {
+
+    Genre create(Genre aGenre);
+
+    Pagination<Genre> findAll(SearchQuery aQuery);
+
+    Optional<Genre> findById(GenreID anId);
+
+    Genre update(Genre aGenre);
+
+    void deleteById(GenreID anId);
+
+}
