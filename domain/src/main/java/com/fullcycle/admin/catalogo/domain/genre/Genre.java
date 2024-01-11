6fbcbd7e04bf7cf161e358b7248073372a95de79
Wesley Pereira <jimmmisss@gmail.com>
2024-01-11 07:31:51 -0300

    Full Cycle 3 - Domain Genre - TDD Genre

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/genre/Genre.java
@@ -0,0 +1,127 @@
+package com.fullcycle.admin.catalogo.domain.genre;
+
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.domain.exceptions.NotificationException;
+import com.fullcycle.admin.catalogo.domain.utils.InstantUtils;
+import com.fullcycle.admin.catalogo.domain.validation.ValidationHandler;
+import com.fullcycle.admin.catalogo.domain.validation.handler.Notification;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Genre extends AggregateRoot<GenreID> {
+
+    private String name;
+    private boolean active;
+    private List<CategoryID> categories;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    protected Genre(
+            final GenreID anId,
+            final String aName,
+            final boolean isActive,
+            final List<CategoryID> categories,
+            final Instant aCreatedAt,
+            final Instant aUpdatedAt,
+            final Instant aDeletedAt
+    ) {
+        super(anId);
+        this.name = aName;
+        this.categories = categories;
+        this.active = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = aUpdatedAt;
+        this.deletedAt = aDeletedAt;
+        selfValidate();
+    }
+
+    public static Genre newGenre(final String aName, final boolean isActive) {
+        final var anId = GenreID.unique();
+        final var now = InstantUtils.now();
+        final var deletedAt = isActive ? null : now;
+        return new Genre(anId, aName, isActive, new ArrayList<>(), now, now, deletedAt);
+    }
+
+    public Genre with(
+            final GenreID anId,
+            final String aName,
+            final boolean isActive,
+            final List<CategoryID> categories,
+            final Instant aCreatedAt,
+            final Instant aUpdatedAt,
+            final Instant aDeletedAt
+    ) {
+        return new Genre(anId, aName, isActive, categories, aCreatedAt, aUpdatedAt, aDeletedAt);
+    }
+
+    public Genre with(Genre aGenre) {
+        return new Genre(
+                aGenre.getId(),
+                aGenre.getName(),
+                aGenre.isActive(),
+                new ArrayList<>(aGenre.getCategories()),
+                aGenre.createdAt,
+                aGenre.updatedAt,
+                aGenre.deletedAt
+        );
+    }
+
+    private void selfValidate() {
+        final var notification = Notification.create();
+        validate(notification);
+
+        if (notification.hasError()) {
+            throw new NotificationException("Failed to create a Aggregate Genre", notification);
+        }
+    }
+
+    @Override
+    public void validate(ValidationHandler handler) {
+        new GenreValidator(this, handler).validate();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public List<CategoryID> getCategories() {
+        return Collections.unmodifiableList(categories);
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public Genre deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = InstantUtils.now();
+        }
+        this.active = false;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+
+    public Genre activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = InstantUtils.now();
+        return this;
+    }
+}
