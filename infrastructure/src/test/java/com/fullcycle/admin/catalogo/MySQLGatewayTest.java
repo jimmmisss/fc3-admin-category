482eafc9066d9148e80f18a6aaa37a0deddbc46c
Wesley Pereira <jimmmisss@gmail.com>
2024-01-21 06:58:18 -0300

    Full Cycle 3 - Infrastructure Genre - Fixing annotation problem

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/MySQLGatewayTest.java
@@ -3,20 +3,23 @@ package com.fullcycle.admin.catalogo;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.lang.annotation.*;
+
+import static org.springframework.context.annotation.FilterType.REGEX;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test-integration")
-@ComponentScan(includeFilters = {
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*[MySQLGateway]")
-})
+@ComponentScan(
+        basePackages = "com.fullcycle.admin.catalogo",
+        includeFilters = {
+                @ComponentScan.Filter(type = REGEX, pattern = ".[MySQLGateway]")
+        }
+)
 @DataJpaTest
-@ExtendWith(MySQLGatewayTest.CleanUpExtensions.class)
+@ExtendWith(MySQLCleanUpExtension.class)
 public @interface MySQLGatewayTest {
-
 }
